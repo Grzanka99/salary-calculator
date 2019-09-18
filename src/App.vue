0e1714f752243db7a450bccdf9d66f0d0fdeb231
Cezary Grzanka <cezarygrzanka@gmail.com>
2019-09-18 22:22:18 +0200

    Create basics

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,24 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap');
 
- *, *::before, *::after {
-   box-sizing: border-box;
- }
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
 
- body {
-   font-family: 'Roboto', sans-serif;
-   padding: 0;
-   margin: 0;
-   font-size: 62.5%;
-   background-color: #424242;
- }
+body {
+  font-family: 'Roboto', sans-serif;
+  padding: 0;
+  margin: 0;
+  font-size: 62.5%;
+  background-color: #616161;
+  color: white;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,37 @@
 <template>
   <div class="home">
-    <div class="info">
-      <div class="info__username">Your Name</div>
-      <div class="info__amount">{{ amount }}</div>
-    </div>
+    <header class="header">
+      <div class="info">
+        <div class="info__username">Your Name</div>
+        <div class="info__amount">{{ amount }} zł</div>
+      </div>
+    </header>
+    <main class="content">
+      <form>
+        <table class="table">
+          <tr class="table__row">
+            <td class="table__field-id table__field">ID</td>
+            <td class="table__field-for table__field">Za co</td>
+            <td class="table__field-time table__field">Czas (min)</td>
+            <td class="table__field-rate table__field">Stawka (zł/h)</td>
+            <td class="table__field-info table__field">Dodatkowe informacje</td>
+          </tr>
+
+          <tr class="table__row" v-for="item in currData" :key="item.id">
+            <td class="table__field-id table__field">{{ item.id }}</td>
+            <td class="table__field-for table__field">
+              <input type="text" :value="item.for" readonly />
+            </td>
+            <td class="table__field-time table__field">{{ item.time }} min</td>
+            <td class="table__field-rate table__field">{{ item.rate }} zł/h</td>
+            <td class="table__field-info table__field">
+              <input type="text" :value="item.info ? item.info : 'Brak'" readonly />
+            </td>
+            <td class="table__field-del table__field"></td>
+          </tr>
+        </table>
+      </form>
+    </main>
   </div>
 </template>
 
@@ -12,20 +40,191 @@ export default {
   name: 'home',
   data() {
     return {
-      amount: 200,
+      currData: [
+        {
+          id: 1,
+          for: 'Backup - Haier-ac',
+          time: 30,
+          rate: 35,
+          info: undefined
+        },
+        {
+          id: 2,
+          for: 'Intech - Part 5',
+          time: 105,
+          rate: 35,
+          info: 'poprawić to i tamto'
+        }
+      ]
+    };
+  },
+  computed: {
+    amount() {
+      let amount = 0;
+
+      this.currData.forEach(el => {
+        const per15 = el.rate / 4;
+        const workTimeCount = Math.ceil(el.time / 15);
+        amount += workTimeCount * per15;
+      });
+
+      return amount;
     }
   }
 };
 </script>
 
 <style lang="scss" scoped>
-  .home {
-    font-size: 1.6em;
+input {
+  width: 100%;
+  height: 100%;
+  background-color: transparent;
+  border: none;
+  color: white;
+  font-size: 1em;
+  padding: 2px 15px;
+}
+
+.home {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.header {
+  display: flex;
+  align-items: center;
+  width: calc(100% - 30px);
+  height: 150px;
+}
+
+.info {
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 620px;
+  height: 100px;
+  border-radius: 5px;
+
+  background-color: #424242;
+
+  &__username,
+  &__amount {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 
-  .info {
-    display: flex;
-    
-    &__username {}
+  &__username {
+    font-size: 4.4em;
+    left: 20px;
   }
+
+  &__amount {
+    font-size: 4em;
+    font-weight: bold;
+    right: 20px;
+    width: 200px;
+    height: 70px;
+    border-radius: 5px;
+    background-color: #d84315;
+  }
+}
+
+.content {
+  width: calc(100% - 30px);
+  height: calc(100vh - 250px);
+  background-color: #424242;
+  border-radius: 5px;
+  font-size: 2.2em;
+
+  form {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+}
+
+.table {
+  position: relative;
+  top: 30px;
+  width: calc(100% - 30px);
+
+  &__row {
+    position: relative;
+    display: flex;
+    justify-content: space-between;
+    height: 40px;
+    width: 100%;
+    border: none;
+    margin: 5px 0;
+
+    &:first-child {
+      margin: 0 0 15px;
+    }
+  }
+
+  &__field {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden;
+    border: none;
+    border-radius: 5px;
+    background-color: #212121;
+    flex: 1;
+    margin: 0 10px;
+
+    &-id {
+      min-width: 40px;
+      max-width: 40px;
+    }
+
+    &-for {
+      min-width: 200px;
+      max-width: 200px;
+    }
+
+    &-time,
+    &-rate {
+      min-width: 150px;
+      max-width: 150px;
+    }
+
+    &-del {
+      min-width: 40px;
+      max-width: 40px;
+      background-color: #e65100;
+      border: 3px solid #212121;
+      transition: background-color 200ms 10ms ease-in-out;
+
+      &:hover {
+        background-color: #ef6c00;
+      }
+
+      &::before,
+      &::after {
+        position: absolute;
+        content: '';
+        display: block;
+        width: 90%;
+        height: 5px;
+        background-color: #212121;
+      }
+
+      &::before {
+        transform: rotate(45deg);
+      }
+
+      &::after {
+        transform: rotate(-45deg);
+      }
+    }
+  }
+}
 </style>
